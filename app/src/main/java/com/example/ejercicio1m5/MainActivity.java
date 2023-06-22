901e5d76ee4978f5964eabb42991d025be3406eb
ArellanoFrancisco <francisco.arellano.salazar@gmail.com>
2023-06-22 02:30:14 -0400

    se agrega la barra de busqueda con su boton ir, y ademas el boton de salir

--- app/src/main/java/com/example/ejercicio1m5/MainActivity.java
+++ app/src/main/java/com/example/ejercicio1m5/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.ejercicio1m5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+import com.example.ejercicio1m5.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+ private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = binding.url.getEditableText().toString();
+                Fragmentwebview fragment = (Fragmentwebview) getSupportFragmentManager().findFragmentById(R.id.fragmento);
+                WebView webView = fragment.getView().findViewById(R.id.Web1);
+                webView.loadUrl(url);
+            }
+        });
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+            }
+        });
+
     }
 }
